f660bad536c3aadc1cc3f596aa1d70f986b05e85
murad_0799 <murodbest19@gmail.com>
2022-01-31 16:58:11 +0500

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar
+    app
+  >
+    <div class="d-flex align-center"><h2>Where in the world?</h2></div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text @click="darkMode">
+      <font-awesome-icon
+        v-if="$vuetify.theme.dark"
+        icon="sun"
+        size="lg"
+      />
+      <font-awesome-icon v-else icon="moon" size="lg"/>
+      <span class="ml-2">
+        {{ $vuetify.theme.dark ? 'Light Mode' : 'Dark Mode' }}</span
+      >
+    </v-btn>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  methods: {
+    darkMode () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
